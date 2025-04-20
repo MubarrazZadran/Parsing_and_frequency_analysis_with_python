5d56189b54dd90157ae68e56b47e93ced1d98594
MubarrazZadran <mubarraz.zadran@gmail.com>
2025-04-20 14:37:53 -0400

    Add files via upload

---
+++ Practise_Dictionaries_5.py
@@ -0,0 +1,24 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+dic = dict()
+for x in handle:
+    if not x.startswith("From "): continue
+    x  = x.split()
+    x = x[5]
+    x = x.split(":")
+    x = x[0]
+    dic[x] = dic.get(x,0)+1
+print(dic)
+lst = list()
+for x,y in dic.items():
+    tup = (x,y)
+    lst.append(tup)
+print(lst)
+lsst = sorted(lst)
+for l,t in lsst[:]:
+    print(l,t)
+    
+    #ls.append(values)
+    #print(sorted(ls))
